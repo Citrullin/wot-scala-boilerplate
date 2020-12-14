a75220c4b2c20167a949c55220e94c609adf51ac
Philipp Blum <philipp-blum@jakiku.de>
2020-12-14 09:42:13 +0100

    Add try catch

--- src/main/scala/com/github/citrullin/Main.scala
+++ src/main/scala/com/github/citrullin/Main.scala
@@ -7,14 +7,19 @@ import city.sane.wot.thing._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val wot: Wot = new DefaultWot()
+    val wot: Wot = DefaultWot.clientOnly()
 
-    val thing = wot.fetch(new URI("coap://[fe80::9c7c:12ff:fe70:8f55%tapbr0]/.well-known/wot-thing-description")).get();
-    thing.getDescriptions().asScala.map{case (key, value) => {
-        println(key + ":" + value)
+    try {
+      val thing = wot.fetch(new URI("coap://[fe80::9c7c:12ff:fe70:8f55%tapbr0]/.well-known/wot-thing-description")).get()
+      thing.getProperties().asScala.map{case (key, _) => {
+          println(key)
+        }
       }
+      val consumedThing: ConsumedThing = wot.consume(thing);
+    } catch {
+      case _ : Throwable => println("Error while fetching the Thing description")
     }
-    val consumedThing: ConsumedThing = wot.consume(thing);
+
     ()
   }
 }
