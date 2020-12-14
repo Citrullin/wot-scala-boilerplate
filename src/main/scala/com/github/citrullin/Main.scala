d6e756f6ac5e28c6092aa3ca1ba9010455be65e7
Philipp Blum <philipp-blum@jakiku.de>
2020-12-14 02:41:18 +0100

    Remove comment, because rtfm, coap is supported

--- src/main/scala/com/github/citrullin/Main.scala
+++ src/main/scala/com/github/citrullin/Main.scala
@@ -1,5 +1,6 @@
 package com.github.citrullin
 
+import scala.jdk.CollectionConverters._
 import java.net.URI
 import city.sane.wot._
 import city.sane.wot.thing._
@@ -7,8 +8,12 @@ import city.sane.wot.thing._
 object Main {
   def main(args: Array[String]): Unit = {
     val wot: Wot = new DefaultWot()
-    //coap not supported
+
     val thing = wot.fetch(new URI("coap://[fe80::9c7c:12ff:fe70:8f55%tapbr0]/.well-known/wot-thing-description")).get();
+    thing.getDescriptions().asScala.map{case (key, value) => {
+        println(key + ":" + value)
+      }
+    }
     val consumedThing: ConsumedThing = wot.consume(thing);
     ()
   }
